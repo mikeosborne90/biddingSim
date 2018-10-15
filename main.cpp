1901354460f2c74a8c3861f7794c59bd9f8b23c0
Michael Osborne <unfazed90@mail.fresnostate.edu>
2018-10-14 17:01:40 -0700

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+//CSCI 156 Bidding Simulation Project
+//By: Michael Osborne & Victor Lopez
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"This is a bidding simulation project using socket programming."<<endl;
+
+    return 0;
+}
